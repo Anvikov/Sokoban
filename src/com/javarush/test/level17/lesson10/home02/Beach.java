299291c92876f79abe5c43aba635a8c5807a3766
User <dormammu1@gmail.com>
2016-05-04 19:10:09 +0300

    _

--- src/com/javarush/test/level17/lesson10/home02/Beach.java
+++ src/com/javarush/test/level17/lesson10/home02/Beach.java
@@ -1,46 +1,67 @@
 package com.javarush.test.level17.lesson10.home02;
+
 /* Comparable
 Реализуйте интерфейс Comparable<Beach> в классе Beach, который будет использоваться нитями.
 */
-public class Beach implements Comparable<Beach>
-{
+
+import java.util.TreeSet;
+
+public class Beach implements Comparable<Beach>{
     private String name;      //название
     private float distance;   //расстояние
     private int quality;    //качество
-    public Beach(String name, float distance, int quality)
-    {
+
+    public  Beach(String name, float distance, int quality) {
         this.name = name;
         this.distance = distance;
         this.quality = quality;
     }
-    public String getName()
-    {
+
+    public synchronized  String getName() {
         return name;
     }
-    public void setName(String name)
-    {
+
+    public synchronized  void setName(String name) {
         this.name = name;
     }
-    public float getDistance()
-    {
+
+    public synchronized  float getDistance() {
         return distance;
     }
-    public void setDistance(float distance)
-    {
+
+    public synchronized  void setDistance(float distance) {
         this.distance = distance;
     }
-    public int getQuality()
-    {
+
+    public synchronized  int getQuality() {
         return quality;
     }
-    public void setQuality(int quality)
-    {
+
+    public synchronized  void setQuality(int quality) {
         this.quality = quality;
     }
+
     @Override
-    public synchronized int compareTo(Beach obj)
+    public synchronized int compareTo(Beach o)
     {
-        int distIndex = (int) (distance - obj.getDistance());
-        int qualIndex = quality - obj.getQuality();
-        return 10000 * name.compareTo(obj.getName()) + 100 * distIndex + qualIndex;
-    }}
+        int distan = (int)(distance - o.getDistance());
+        int qual = (int)(quality - o.quality);
+        return 10000 * name.compareTo(o.getName()) + 100 * distan + qual;
+    }
+    public static void main(String[] args){
+
+        TreeSet<Beach> beachSet = new TreeSet<Beach>();
+
+        beachSet.add(new Beach("beach1", 125.6f, 7));
+        beachSet.add(new Beach("beach2", 124.6f, 8));
+        beachSet.add(new Beach("beach3", 124.6f, 7));
+        beachSet.add(new Beach("beach4", 124.2f, 7));
+        beachSet.add(new Beach("beach5", 124.6f, 9));
+        beachSet.add(new Beach("beach6", 124.6f, 6));
+
+        for (Beach x : beachSet)
+        {
+            System.out.println(x.getName() + " " + x.getDistance() + " " + x.getQuality());
+        }
+    }
+}
